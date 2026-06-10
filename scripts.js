4d5a73a4a264badd78927a1ae57456ef45eecee0
Isabelle Joubert <isabelleelenajoubert@gmail.com>
2026-06-10 15:34:12 +0200

    Create humanChoice and playRound logic

--- scripts.js
+++ scripts.js
@@ -15,8 +15,69 @@ function getComputerChoice() {
         return `The computer chose paper!`;
     }
     else {
-        return `The computer chose scissors`;
+        return `The computer chose scissors!`;
     }
 }
 
-console.log(getComputerChoice())
+// Now we need to get the human's choice
+// The first thing to do is take their input through a screen prompt
+// then we want to store that in the javascript somehow
+// then we want to make sure their input is in lower case so there aren't any issues with case sensitivity
+// then we want to store that in a variable that the function returns that can be passed as a parameter later on
+// We also want to make sure the prompt retriggers when the old prompt is closed so we will need to add a javascript event listener
+
+function getHumanChoice() {
+    let input = prompt("Rock, paper, or scissors?");
+    let inputLowerCase = input.toLowerCase();
+    return inputLowerCase
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+// Now we need a function that compares the players' selections and decides on a winner
+// rock beats scissors
+// scissors beats paper
+// paper beats rock
+// there will need to be if, else statements to account for all of these possibilities
+// we also need to use += to increase the scores of either player depending on what they play
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock' && computerChoice === "paper") {
+        computerScore += 1;
+        return `You played rock and computer played paper. Computer wins! Current score is human, ${humanScore}, computer, ${computerScore}!`
+    }
+    else if (humanChoice === 'rock' && computerChoice === "scissors") {
+        humanScore += 1;
+        return `You played rock and computer played scissors. You win! Current score is human, ${humanScore}, computer, ${computerScore}!`
+    }
+    else if (humanChoice === 'rock' && computerChoice === "rock") {
+        return `You played rock and computer played rock. It's a draw! Current score is human, ${humanScore}, computer, ${computerScore}!`
+    }
+    else if (humanChoice === 'paper' && computerChoice === "rock") {
+        humanScore += 1;
+        return `You played paper and computer played rock. You win! Current score is human, ${humanScore}, computer, ${computerScore}!`
+    }
+    else if (humanChoice === 'paper' && computerChoice === "scissors") {
+        computerScore += 1;
+        return `You played paper and computer played scissors. Computer wins! Current score is human, ${humanScore}, computer, ${computerScore}!`
+    }
+    else if (humanChoice === 'paper' && computerChoice === "paper") {
+        return `You played paper and computer played paper. It's a draw! Current score is human, ${humanScore}, computer, ${computerScore}!`
+    }
+    else if (humanChoice === 'scissors' && computerChoice === "rock") {
+        computerScore += 1;
+        return `You played scissors and computer played rock. Computer wins! Current score is human, ${humanScore}, computer, ${computerScore}!`
+    }
+    else if (humanChoice === 'scissors' && computerChoice === "scissors") {
+        return `You played paper and computer played scissors. It's a draw! Current score is human, ${humanScore}, computer, ${computerScore}!`
+    }
+    else if (humanChoice === 'scissors' && computerChoice === "paper") {
+        humanScore += 1;
+        return `You played paper and computer played paper. You win! Current score is human, ${humanScore}, computer, ${computerScore}!`
+    }
+}
+
+// console.log(getComputerChoice())
+console.log(getHumanChoice())
+// console.log(playRound(getHumanChoice(), getComputerChoice()))
